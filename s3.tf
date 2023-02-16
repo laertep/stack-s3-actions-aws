21601cd0dc799fb9e0fc79abef26ab6f71c7c63e
Laerte Porto Júnior <44216245+laertep@users.noreply.github.com>
2023-02-16 09:53:50 -0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -7,6 +7,18 @@ terraform {
   }
  }
  
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
 
+variable "access_key" {
+  default = "<hidden_for_stack_exchange_post>"
+}
+
+variable "secret_key" {
+  default = "<hidden_for_stack_exchange_post>"
+}
  
 
