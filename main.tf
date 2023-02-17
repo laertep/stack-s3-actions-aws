61a68bfad5e41cd02624cf5e36dcb923715e4a15
Laerte Porto Júnior <44216245+laertep@users.noreply.github.com>
2023-02-17 16:05:44 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.websitelaerte.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
+
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
     principals {
