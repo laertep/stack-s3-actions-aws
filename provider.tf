1ea2bf49d791a39cde3592f403355a3a59378c2e
Laerte Porto Júnior <44216245+laertep@users.noreply.github.com>
2023-02-16 21:44:15 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,18 @@
 terraform {
+  required_version = "1.3.6"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~>4.0"
     }
-  }
-}
+
+
+
+
+
+
+
 
 
 
