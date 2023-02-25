8e2e8221877de0b54531654e3f8802c9dad102cf
Laerte Porto Júnior <44216245+laertep@users.noreply.github.com>
2023-02-25 17:05:57 -0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -8,15 +8,4 @@ terraform {
 
 
 
-resource "aws_s3_bucket" "terraformlaertes3" {
-  bucket = "terraformlaertes3"
-}
 
-resource "aws_s3_bucket_public_access_block" "terraformlaertes3" {
-  bucket = aws_s3_bucket.terraformlaertes3.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
